281979fcbdce975c093998022df696c781b211af
Fatih-Tarim <fatihtrm99@gmail.com>
2023-11-09 18:13:18 +0300

    Problem-1

---
+++ project-eular/problem-1.py
@@ -0,0 +1,20 @@
+# Mulitple 3 or 5
+# If we list all the natural numbers below $10$ that are multiples of $3$ or $5$, we get $3, 5, 6$ and $9$. The sum of these multiples is $23$.</p>
+# Find the sum of all the multiples of $3$ or $5$ below $1000$.
+
+def check(x):
+    if x%5 == 0 or x%3 == 0:
+        return True
+    else:
+        return False
+    
+sum = 0
+
+for i in range(1,1000):
+    if check(i) == True:
+        sum += i
+
+print(sum)
+    
+
+    
